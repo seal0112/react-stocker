cecb75fa0223e8c6740d5617c291bb66b95fdb7a
seal0112 <seal0112@gmail.com>
2020-03-06 21:35:27 +0800

    [chore] add react-bootstrap package

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Button } from 'react-bootstrap';
 
 function App() {
   return (
@@ -19,6 +20,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <Button color="primary">primary</Button>
     </div>
   );
 }
