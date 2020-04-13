05ada70f4420b5583d6713d4255e2763f439f5a2
seal0112 <seal0112@gmail.com>
2020-04-13 18:14:07 +0800

    [fix] change main content's container position

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import logo from './logo.svg';
 import './App.css';
 import Header from './Header';
 import NaviBar from './NaviBar';
+import DailyInfo from './DailyInfo'
 import { Route, Switch } from "react-router-dom";
+import { Container } from 'react-bootstrap';
 
 class StockerApp extends Component {
   state = {
@@ -21,24 +23,32 @@ class StockerApp extends Component {
         <div className="App">
             <Header handleStockNumChange={this.handleStockNumChange} />
             <NaviBar />
-            <p>{this.state.stockNum}</p>
-            <Switch>
-                <Route path="/daily-info">
-                    <p>daily-info</p>
-                </Route>
-                <Route path="/company-data">
-                    <p>company data</p>
-                </Route>
-                <Route path="/news">
-                    <p>news</p>
-                </Route>
-                <Route path="/comment">
-                    <p>comment</p>
-                </Route>
-                <Route path="*">
-                    <p>no match</p>
-                </Route>
-            </Switch>
+            <Container>
+                <h3>{this.state.stockNum} 台積電</h3>
+                <hr />
+                <Switch>
+                    {["/", "/daily-info"].map(path => (
+                        <Route
+                          key={path}
+                          exact
+                          path={path}>
+                            <DailyInfo />
+                        </Route>
+                      ))}
+                    <Route path="/company-data">
+                        <p>company data</p>
+                    </Route>
+                    <Route key="news" path="/news">
+                        <p>news</p>
+                    </Route>
+                    <Route key="comment" path="/comment">
+                        <p>comment</p>
+                    </Route>
+                    <Route path="*">
+                        <p>no match</p>
+                    </Route>
+                </Switch>
+            </Container>
         </div>
     )
   };
