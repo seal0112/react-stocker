d815b2e7c1fac1c038bd6269930a644ca43a5f02
seal0112 <seal0112@gmail.com>
2020-04-28 19:57:59 +0800

    [feat] let table overflow with scroll bar and fix header column

--- src/CustomizedTable.js
+++ src/CustomizedTable.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import './CustomizedTable.css';
 import {
     Table
 } from 'react-bootstrap';
@@ -12,35 +13,43 @@ class CustomizedTable extends Component {
     render() {
         return (
             <div class="Customized-table">
-                <Table striped bordered hover responsive="md">
-                  <thead>
-                    <tr>
-                      {this.props.data.map(data=>(<th>{data[0]}</th>))}
-                    </tr>
-                  </thead>
-                  <tbody>
-                    {this.props.data[0].map((d, index) => {
-                        if (index===0) {
-                            return;
-                        } else {
-                            return (
-                            <tr>
-                                {this.props.data.map(
-                                    (rowData, rowIdx)=>{
-                                        if(rowIdx===0){
-                                            return (<th className="head-col">{rowData[index]}</th>)
-                                        } else {
-                                            return (<td>{
-                                                typeof(rowData[index])=='number'?this.numberWithCommas(rowData[index]):rowData[index]
-                                            }</td>)
-                                        }
-                                    })
+                <div class="table-scroller">
+                    <Table striped bordered hover responsive="md">
+                      <thead>
+                        <tr>
+                            {this.props.data.map((data, index)=>{
+                                if (index===0){
+                                    return (<th className="head-col">{data[0]}</th>)
+                                }else{
+                                    return (<th>{data[0]}</th>)
                                 }
-                            </tr>);
-                        }
-                    })}
-                  </tbody>
-                </Table>
+                            })}
+                        </tr>
+                      </thead>
+                      <tbody>
+                        {this.props.data[0].map((d, index) => {
+                            if (index===0) {
+                                return;
+                            } else {
+                                return (
+                                <tr>
+                                    {this.props.data.map(
+                                        (rowData, rowIdx)=>{
+                                            if(rowIdx===0){
+                                                return (<th className="head-col">{rowData[index]}</th>)
+                                            } else {
+                                                return (<td>{
+                                                    typeof(rowData[index])=='number'?this.numberWithCommas(rowData[index]):rowData[index]
+                                                }</td>)
+                                            }
+                                        })
+                                    }
+                                </tr>);
+                            }
+                        })}
+                      </tbody>
+                    </Table>
+                </div>
             </div>
         )
     }
