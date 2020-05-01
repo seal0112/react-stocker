354123e5b3aa8b393f551d4974a81babfdc465c1
seal0112 <seal0112@gmail.com>
2020-05-01 19:17:32 +0800

    [fix] fix <tr>, <th>, <td> without unique key in CustomizedTable.js

--- src/CustomizedTable.js
+++ src/CustomizedTable.js
@@ -12,33 +12,31 @@ class CustomizedTable extends Component {
 
     render() {
         return (
-            <div class="Customized-table">
-                <div class="table-scroller">
+            <div className="Customized-table">
+                <div className="table-scroller">
                     <Table striped bordered hover responsive="md">
                       <thead>
-                        <tr>
-                            {this.props.data.map((data, index)=>{
-                                if (index===0){
-                                    return (<th className="head-col">{data[0]}</th>)
-                                }else{
-                                    return (<th>{data[0]}</th>)
-                                }
-                            })}
+                        <tr key="time">
+                            {this.props.data.map((data, index)=>
+                                (<th className={index===0?"head-col":""} key={`time-${data[0]}`}>{data[0]}</th>)
+                            )}
                         </tr>
                       </thead>
                       <tbody>
-                        {this.props.data[0].map((d, index) => {
-                            if (index===0) {
-                                return;
-                            } else {
+                        {this.props.data[0].forEach((d, index) => {
+                            if (index!==0) {
                                 return (
-                                <tr>
+                                <tr key={`${d}`}>
                                     {this.props.data.map(
                                         (rowData, rowIdx)=>{
                                             if(rowIdx===0){
-                                                return (<th className="head-col">{rowData[index]}</th>)
+                                                return (<th
+                                                          className="head-col"
+                                                          key={`${d}-${rowData[0]}`}>
+                                                            {rowData[index]}
+                                                        </th>)
                                             } else {
-                                                return (<td>{
+                                                return (<td key={`${d}-${rowData[0]}`}>{
                                                     typeof(rowData[index])=='number'?this.numberWithCommas(rowData[index]):rowData[index]
                                                 }</td>)
                                             }
