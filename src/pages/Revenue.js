2f149571e5663c1f8b462b6556c97a027d948498
Raymond Hu <seal0112@gmail.com>
2023-09-08 13:57:12 +0800

    feat: month revenue add monthly growth rate

--- src/pages/Revenue.js
+++ src/pages/Revenue.js
@@ -9,8 +9,8 @@ import * as StockerTool from '../utils/StockerTool'
 /*
  * Revenue Data example for google chart
  * revenueData: [
- *     ["Year/Month", "當月營收", "去年同月增減"],
- *     ["2013/1", 733.044, 22.6],
+ *     ["Year/Month", "當月營收", "去年同月增減", "上月比較增減"],
+ *     ["2013/1", 733.044, 22.6, 3.06],
  * ]
  */
 const Revenue = () => {
@@ -20,7 +20,7 @@ const Revenue = () => {
   ])
   const [activeKey, setActiveKey] = useState('precentageOperExp')
   const stock = useStock()
-  const revenueKeysOrder = ['Year/Month', '當月營收', '去年同月增減']
+  const revenueKeysOrder = ['Year/Month', '當月營收', '去年同月增減', '上月比較增減']
 
   const handleCount = (key) => {
     setActiveKey(key)
@@ -37,8 +37,10 @@ const Revenue = () => {
   }, [stock.stockNum])
 
   const revenue = revenueData.map(d => [].concat(d.slice(0, 2)))
-  const annualIncrease = revenueData.map(d => [].concat(d.slice(0, 1), d.slice(2)))
+  const annualIncrease = revenueData.map(d => [].concat(d.slice(0, 1), d.slice(2, 3)))
+  const monthIncrease = revenueData.map(d => [].concat(d.slice(0, 1), d.slice(3)))
   console.log(activeKey)
+
   return (
     <div className="revenue">
       <Tabs defaultActiveKey="revenue" id="Revenue-Analysis-tab" onSelect={handleCount}>
@@ -68,14 +70,14 @@ const Revenue = () => {
             }}
             data={revenue} />
         </Tab>
-        <Tab eventKey="annualIncrease" title="去年同月增減">
+        <Tab eventKey="annualIncrease" title="營收年增率">
           <Chart
             chartType="ComboChart"
             width="100%"
             height="500px"
             loader={<div>Loading Chart</div>}
             options={{
-              title: '去年同月增減',
+              title: '營收年增率',
               legend: {
                 position: 'top'
               },
@@ -91,6 +93,30 @@ const Revenue = () => {
               hAxis: {}
             }}
             data={annualIncrease} />
+        </Tab>
+        <Tab eventKey="monthIncrease" title="營收月增率">
+          <Chart
+            chartType="ComboChart"
+            width="100%"
+            height="500px"
+            loader={<div>Loading Chart</div>}
+            options={{
+              title: '營收月增率',
+              legend: {
+                position: 'top'
+              },
+              chartArea: { width: '75%' },
+              seriesType: 'line',
+              series: {
+                0: { visibleInLegend: true, color: 'red' }
+              },
+              pointSize: 7,
+              vAxes: {
+                0: {}
+              },
+              hAxis: {}
+            }}
+            data={monthIncrease} />
         </Tab>
       </Tabs>
       <CustomizedTable data={revenueData}/>
